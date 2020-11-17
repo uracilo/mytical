2a779a8c459786a411fbc075cba205ec2fe3c163
Benjamin <benjamin.casazza@gmail.com>
2020-11-17 16:04:41 -0600

    remove files

--- p/lib/python3.8/subprocess.py
+++
@@ -1,1 +0,0 @@
-/home/linuxbrew/.linuxbrew/Cellar/python@3.8/3.8.6/lib/python3.8/subprocess.py
--- tests/test_app.py
+++
@@ -1,8 +0,0 @@
-import json
-
-
-def test_index(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
-    expected = 'used for health check'
-    assert expected in json.loads(res.get_data(as_text=True))['message']
